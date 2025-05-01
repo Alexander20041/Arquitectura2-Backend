6a0014a7e00f75e5ec71d9be4a2ab77dcd106990
Mpinzon222 <mpinzonh1@ucentral.edu.co>
2025-05-01 18:26:19 -0500

    Metodos para modificar y eliminar alertas

--- src/main/java/com/example/FinazApp/controladores/ControladorAlerta.java
+++ src/main/java/com/example/FinazApp/controladores/ControladorAlerta.java
@@ -7,6 +7,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping("/Finanzapp/Alertas")
@@ -21,4 +23,25 @@ public class ControladorAlerta {
         AlertaDTO alertaRegistrada = servicioAlerta.RegistrarAlerta(alerta, id_usuario);
         return (alertaRegistrada != null) ? ResponseEntity.ok(alertaRegistrada) : ResponseEntity.badRequest().build();
     }
+
+
+
+    @GetMapping("/ObtenerAlertaPorUser/{id_usuario}")
+    public ResponseEntity<List<AlertaDTO>> listarAlertaPorUser(@PathVariable Long id_usuario) {
+        List<AlertaDTO> alertas = servicioAlerta.ObtenerAlerta(id_usuario);
+        return ResponseEntity.ok(alertas);
+    }
+
+    @GetMapping("/ObtenerAlertaPorAnio/{id_usuario}")
+    public ResponseEntity<List<AlertaDTO>> listarAlertaPorAnio(@PathVariable Long id_usuario) {
+        List<AlertaDTO> alertas = servicioAlerta.ObtenerAlertaFecha(id_usuario);
+        return ResponseEntity.ok(alertas);
+    }
+
+    @GetMapping("/ObtenerAlertaPorMes/{id_usuario}")
+    public ResponseEntity<List<AlertaDTO>> listarAlertaPorMes(@PathVariable Long id_usuario) {
+        List<AlertaDTO> alertas = servicioAlerta.ObtenerAlertaEsteMes(id_usuario);
+        return ResponseEntity.ok(alertas);
+    }
+
 }
--- src/main/java/com/example/FinazApp/servicios/ServicioAlerta.java
+++ src/main/java/com/example/FinazApp/servicios/ServicioAlerta.java
@@ -11,6 +11,10 @@ import lombok.Data;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 @Data
 @AllArgsConstructor
@@ -33,6 +37,79 @@ public class ServicioAlerta {
         Alerta AlertaGuardada = repositorioAlerta.save(nuevaAlerta);
 
         return modelMapper.map(AlertaGuardada, AlertaDTO.class);
+
+    }
+
+
+    public List<AlertaDTO> ObtenerAlerta(Long id_usuario) {
+
+        List<Alerta> alertas  = repositorioAlerta.getAlertasPorUsuario(id_usuario);
+
+        return alertas.stream()
+                .map(alerta -> modelMapper.map(alerta, AlertaDTO.class))
+                .collect(Collectors.toList());
+
+    }
+
+
+    public List<AlertaDTO> ObtenerAlertaFecha(Long id_usuario) {
+
+        List<Alerta> alertas  = repositorioAlerta.getAlertasDeEsteAno(id_usuario);
+
+        return alertas.stream()
+                .map(alerta -> modelMapper.map(alerta, AlertaDTO.class))
+                .collect(Collectors.toList());
+
+    }
+
+
+    public List<AlertaDTO> ObtenerAlertaEsteMes(Long id_usuario) {
+
+        List<Alerta> alertas  = repositorioAlerta.getAlertasDeEsteMes(id_usuario);
+
+        return alertas.stream()
+                .map(alerta -> modelMapper.map(alerta, AlertaDTO.class))
+                .collect(Collectors.toList());
+
+    }
+
+
+    public AlertaDTO ModificarAlerta(Long id_alerta, AlertaDTO alertaDTO) {
+        // Buscar el gasto por su ID en el repositorio
+        Optional<Alerta> AlertaOptional = repositorioAlerta.findById(id_alerta);
+
+        // Validar si la alerta existe
+        if (AlertaOptional.isPresent()) {
+            Alerta alerta = AlertaOptional.get();
+
+            // Actualizar los campos del gasto con los datos del DTO
+            alerta.setNombre(alertaDTO.getNombre());
+            alerta.setDescripcion(alertaDTO.getDescripcion());
+            alerta.setValor(alertaDTO.getValor());
+            alerta.setFecha(alertaDTO.getFecha());
+
+            // Guardar los cambios en el repositorio
+            Alerta AlertaActualizada = repositorioAlerta.save(alerta);
+
+            // Convertir la entidad actualizada de nuevo en un DTO para retornarlo
+            AlertaDTO AlertaActualizadaDTO = new AlertaDTO();
+            AlertaActualizadaDTO.setId_alerta(AlertaActualizada.getId_alerta());
+            AlertaActualizadaDTO.setNombre(AlertaActualizada.getNombre());
+            AlertaActualizadaDTO.setDescripcion(AlertaActualizada.getDescripcion());
+            AlertaActualizadaDTO.setValor(AlertaActualizada.getValor());
+            AlertaActualizadaDTO.setFecha(AlertaActualizada.getFecha());
+
+            return AlertaActualizadaDTO;
+        } else {
+            // Lanza una excepción si el gasto no existe
+            throw new RuntimeException("El gasto con ID " + alertaDTO.getId_alerta() + " no existe.");
+        }
+    }
+
+
+    public void EliminarAlerta (Long id_alerta){
+
+        repositorioAlerta.deleteById(id_alerta);
 
     }
 
